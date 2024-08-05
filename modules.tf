d1df218cf56dbe3dd708a85a4568364ff6d425e1
root <root@pedropc>
2024-08-05 11:19:12 -0300

    Projeto Final

--- modules.tf
+++ modules.tf
@@ -20,18 +20,18 @@ module "producao" {
 
   source = "git::https://github.com/pedrohfborges/terraform-ecs-fargate.git"
 
-    vpc = "192.168.0.0/16"
-    public_subnet = "192.168.30.0/24"
-    netprivate = "192.168.40.0/24"
-    cluster_name = "NomedoClusterTeste"
-    task_definition_family = "my-task-def"
-    service_name = "my-ecs-service"
-    nometarefa = "whoami"
-    containerimage = "stefanscherer/whoami:latest"
-    cpu = 256
-    memory = 512
-    containerport = 8080
-    
+  vpc                    = "192.168.0.0/16"
+  public_subnet          = "192.168.30.0/24"
+  netprivate             = "192.168.40.0/24"
+  cluster_name           = "NomedoClusterTeste"
+  task_definition_family = "my-task-def"
+  service_name           = "my-ecs-service"
+  nometarefa             = "whoami"
+  containerimage         = "stefanscherer/whoami:latest"
+  cpu                    = 256
+  memory                 = 512
+  containerport          = 8080
+
 }
 
 
